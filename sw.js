3ac5546eef7f4fde2c8166ea342dcd0ce400489a
Diego Pagini <dq@qbkconsulting.com>
2021-02-15 13:57:25 -0300

    sw en accion

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 /** @format */
 
-console.log('SW: Hola mundo !');
+//Self hace referencia al mismo.
+//es igual a var self = this;
+self.addEventListener('fetch', (event) => {
+	if (event.request.url.includes('style.css')) {
+		//le quito el style.css a toda la app.
+		event.respondWith(null);
+	}
+	event.respondWith(fetch(event.request));
+});
